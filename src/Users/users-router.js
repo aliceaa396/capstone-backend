63ec5fd4fc880c3136a21730fa24bc6eb0e67c84
Angel Alicea <ala31996@gmail.com>
2019-12-17 09:29:23 -0500

    initial commit

---
+++ src/Users/users-router.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const usersRouter = express.Router()
+const jsonBodyParser = express.json()
+const userService = require('./user-service')
+
+usersRouter.get('/', (req, res) => {
+  res 
+    .status(200)
+    .send('Hello! Endpoint hit!')
+})
+
+usersRouter.post('/register', jsonBodyParser, (req, res) => {
+  console.log(req.body)
+})
+
+usersRouter.post('/login', jsonBodyParser, (req, res)=> {
+  console.log(req.body)
+})
+
+module.exports = usersRouter
