ff0bc43eac99e57ba1124de37df49e4178dd429b
Angel Alicea <ala31996@gmail.com>
2020-01-03 17:33:18 -0500

    fixed eaccess error

--- Seeds/seed.workouts_table.sql
+++ Seeds/seed.workouts_table.sql
@@ -3,21 +3,21 @@ INSERT INTO workouts (exercise_name,workout_set, workout_rep, workout_weight, no
 VALUES 
 (
   'Flat Barbell Bench Press',
-  '1',
-  '6',
-  '225',
+  1,
+  6,
+  225,
   'Went up by 5 pounds'
 ),
 (
   'Incline Dumbell Bench Press',
-  '2',
-  '6',
-  '100',
+  2,
+  6,
+  100,
   'Need to improve depth of movement'
 ),
 (
   '21 Cross Over Cable Flys',
-  '1',
-  '12',
-  '20'
+  1,
+  12,
+  20,
 )
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.NODE_ENV || "postgresql://postgres@localhost/fitness-app",
+  DB_URL: process.env.NODE_ENV || 'postgresql://postgres@localhost/fitness-app',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-};
+}
 
