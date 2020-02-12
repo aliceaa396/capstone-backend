febeb0a03ee2fa24c7318776b4dcf7b641c968b2
Angel Alicea <ala31996@gmail.com>
2020-02-11 22:04:55 -0500

    trying to deploy app

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const knex = require('knex');
 const app = require('./app'); 
 const { PORT, DATABASE_URL } = require('./config');
 
+const PORT = process.env.PORT || 8000
+
 const db = knex({ 
   client: 'pg', 
   connection: DATABASE_URL, 
