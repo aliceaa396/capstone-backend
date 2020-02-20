498f550231c501351d5aa4ab31d8a2fa7e91efc5
Angel Alicea <ala31996@gmail.com>
2020-02-20 13:05:49 -0500

    changed to production from dev

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/fitness-app',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
